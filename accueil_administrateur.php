0dd84e3f7ba0c37a12028d3a5dc6654b3ea4ad7c
AtheneRambach <83444452+AtheneR@users.noreply.github.com>
2024-11-27 15:20:09 +0100

    Create accueil_administrateur.php

---
+++ accueil_administrateur.php
@@ -0,0 +1,33 @@
+<?php
+    include_once('header.php');
+
+    session_start();
+
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: connexion.php');
+        exit();
+    }
+
+    $user_id = $_SESSION['user_id'];
+    $connexionutilisateur = "SELECT * FROM utilisateur WHERE id_utilisateur = :id";
+    $requete_connexion = $bdd->prepare($connexionutilisateur);
+    $requete_connexion->execute(['id' => $user_id]);
+    $user = $requete_connexion->fetch();
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Starvel</title>
+    <link rel="stylesheet" href="visuel.css">
+    <link rel="icon" type="image/png" href="logo_couleur.png">
+</head>
+
+<nav class="navbar">
+    <a href="accueil_utilisateur.php"><img src="logo_couleur.png" alt="Logo Starvel" class="logo" /></a>
+    <a href="reservation.php">Réserver un trajet</a>
+    <a href="modification_utilisateur.php">Modifier mon profil</a>
+    <a href="annulation.php">Mes trajets à venir</a>
+    <a href="historique.php">Historique de mes trajets</a>
+    <a href="connexion.php?action=logout" class="deconnexion">Se déconnecter</a>
+</nav>
