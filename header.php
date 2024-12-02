35d0886a8805cd5fa2ff1117af38869635244b01
AtheneRambach <83444452+AtheneR@users.noreply.github.com>
2024-12-02 13:43:29 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
     try{
         $bdd = new PDO('mysql:host=localhost;dbname=train;charset=utf8', 'root', '');
+        // on configure pour afficher les erreurs de connexion si elles surviennent
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(Exception $e) {
         die('Erreur : '.$e->getMessage());
